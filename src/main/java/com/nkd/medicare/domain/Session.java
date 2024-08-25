52331beed746c2e56591d0795502e6b4e821728c
Piplip <93427122+Piplip@users.noreply.github.com>
2024-08-25 23:03:41 +0700

    adding session mechanism

---
+++ src/main/java/com/nkd/medicare/domain/Session.java
@@ -0,0 +1,10 @@
+package com.nkd.medicare.domain;
+
+import java.time.LocalDateTime;
+
+public class Session {
+
+    private String email;
+    private String sessionID;
+    private LocalDateTime loginTime;
+}
