9bd6c41cd08b9264b2bdc4853749412d1a1dd272
Piplip <93427122+Piplip@users.noreply.github.com>
2024-08-25 23:03:18 +0700

    redis config

---
+++ src/main/java/com/nkd/medicare/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.nkd.medicare.config;
+
+import com.nkd.medicare.domain.Session;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Session> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, Session> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+}
