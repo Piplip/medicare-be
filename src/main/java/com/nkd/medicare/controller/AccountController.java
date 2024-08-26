7b72af9eeb49dd292f44a8ca2cd9d13887235ed9
Piplip <93427122+Piplip@users.noreply.github.com>
2024-08-26 21:40:46 +0700

    adding renew token and exception handling for sign up

--- src/main/java/com/nkd/medicare/controller/AccountController.java
+++ src/main/java/com/nkd/medicare/controller/AccountController.java
@@ -1,6 +1,9 @@
 package com.nkd.medicare.controller;
 
 import com.nkd.medicare.domain.Credential;
+import com.nkd.medicare.exception.ApiException;
+import com.nkd.medicare.exception.DuplicateEmailException;
+import com.nkd.medicare.exception.ExpiredTokenException;
 import com.nkd.medicare.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,16 +23,39 @@ public class AccountController {
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> handleSignUp(@RequestBody Credential credential){
-        accountService.createAccount(credential);
+        try{
+            accountService.createAccount(credential);
+        } catch (DuplicateEmailException duplicate){
+            return ResponseEntity.badRequest().body(duplicate.getMessage());
+        }
+
         return ResponseEntity.ok("Successfully create new account with email " + credential.getEmail());
     }
 
-    @GetMapping("/verify")
-    public ResponseEntity<?> activateAccount(@RequestParam(value = "token") String token){
-        accountService.activateAccount(token);
-        String redirectUrl = "http://localhost:" +clientPort + "/verify/success";
+    @GetMapping("/token/verify")
+    public ResponseEntity<?> activateAccount(@RequestParam(value = "token") String token, @RequestParam(value = "email") String email){
+        try {
+            accountService.activateAccount(token, email);
+        } catch (ApiException | ExpiredTokenException e){
+            String errorUrl = "http://localhost:" + clientPort + "/verify/fail?error=" + e.getMessage();
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .location(java.net.URI.create(errorUrl))
+                    .build();
+        }
+
+        String successUrl = "http://localhost:" + clientPort + "/verify/success";
         return ResponseEntity.status(HttpStatus.FOUND)
-                .location(java.net.URI.create(redirectUrl))
+                .location(java.net.URI.create(successUrl))
                 .build();
+    }
+
+    @PostMapping("/token/renew")
+    public ResponseEntity<?> renewToken(@RequestParam(value = "account") String accountID){
+        try{
+            accountService.renewConfirmationToken(Integer.valueOf(accountID));
+        }catch (ApiException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok("New token has been generated and send to your email! Follow the instructions in the mail to activate your account");
     }
 }
