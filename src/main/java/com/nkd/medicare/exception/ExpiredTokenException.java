8d83be01a87bcf869680f49453fd0611cdc43e1c
Piplip <93427122+Piplip@users.noreply.github.com>
2024-08-26 21:43:55 +0700

    adding new exception for expired token

--- src/main/java/com/nkd/medicare/exception/ExpiredTokenException.java
+++ src/main/java/com/nkd/medicare/exception/ExpiredTokenException.java
@@ -4,4 +4,8 @@ public class ExpiredTokenException extends RuntimeException{
     public  ExpiredTokenException(){
         super("Error! Token is expired");
     }
+
+    public ExpiredTokenException(String accountID){
+        super(accountID);
+    }
 }
