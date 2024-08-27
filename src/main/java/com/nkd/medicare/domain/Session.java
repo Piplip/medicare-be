7a003507429ccac9d4bf12377d628f2016701511
Piplip <93427122+Piplip@users.noreply.github.com>
2024-08-27 23:36:41 +0700

    adding session utils

--- src/main/java/com/nkd/medicare/domain/Session.java
+++ src/main/java/com/nkd/medicare/domain/Session.java
@@ -1,10 +1,23 @@
 package com.nkd.medicare.domain;
 
+import com.nkd.medicare.enums.AccountAccountRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Session {
 
     private String email;
+    private boolean isOnline;
     private String sessionID;
-    private LocalDateTime loginTime;
+    private AccountAccountRole accountRole;
+    private LocalDateTime createdTime;
+    private LocalDateTime lastAccessTime;
+    private LocalDateTime expirationTime;
 }
