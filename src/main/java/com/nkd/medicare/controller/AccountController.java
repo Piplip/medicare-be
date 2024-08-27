0e79e9bc39cb7cd8fd5fe44951753e0a5cbec88a
Piplip <93427122+Piplip@users.noreply.github.com>
2024-08-27 23:33:01 +0700

    adding login

--- src/main/java/com/nkd/medicare/controller/AccountController.java
+++ src/main/java/com/nkd/medicare/controller/AccountController.java
@@ -3,7 +3,6 @@ package com.nkd.medicare.controller;
 import com.nkd.medicare.domain.Credential;
 import com.nkd.medicare.exception.ApiException;
 import com.nkd.medicare.exception.DuplicateEmailException;
-import com.nkd.medicare.exception.ExpiredTokenException;
 import com.nkd.medicare.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,14 +28,37 @@ public class AccountController {
             return ResponseEntity.badRequest().body(duplicate.getMessage());
         }
 
-        return ResponseEntity.ok("Successfully create new account with email " + credential.getEmail());
+        return ResponseEntity.ok("Successfully create new account with email " + credential.getEmail()
+                + ". Please check your email to activate your account");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> handleLogin(@RequestBody Credential credential){
+        Credential sendBackCredential;
+
+        if(credential.getSessionID() != null){
+            try{
+                sendBackCredential = accountService.loginWithSessionID(credential);
+            } catch (ApiException e){
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+            return ResponseEntity.ok(sendBackCredential);
+        }
+
+        try {
+            sendBackCredential = accountService.login(credential);
+        } catch (ApiException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+        return ResponseEntity.ok(sendBackCredential);
     }
 
     @GetMapping("/token/verify")
     public ResponseEntity<?> activateAccount(@RequestParam(value = "token") String token, @RequestParam(value = "email") String email){
         try {
             accountService.activateAccount(token, email);
-        } catch (ApiException | ExpiredTokenException e){
+        } catch (ApiException e){
             String errorUrl = "http://localhost:" + clientPort + "/verify/fail?error=" + e.getMessage();
             return ResponseEntity.status(HttpStatus.FOUND)
                     .location(java.net.URI.create(errorUrl))
