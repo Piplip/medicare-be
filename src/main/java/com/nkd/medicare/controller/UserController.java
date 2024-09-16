27b3e75a551f8c0ec4bc65eaafffb110238d7213
Piplip <93427122+Piplip@users.noreply.github.com>
2024-09-16 15:10:52 +0700

    fixing errors

--- src/main/java/com/nkd/medicare/controller/UserController.java
+++ src/main/java/com/nkd/medicare/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.nkd.medicare.controller;
 
+import com.nkd.medicare.domain.AppointmentDTO;
 import com.nkd.medicare.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +17,17 @@ public class UserController {
     public ResponseEntity<?> getStaffData(@RequestParam("name") String name, @RequestParam("department") String department,
                                           @RequestParam("primary-language") String primaryLanguage, @RequestParam("specialization") String specialization,
                                           @RequestParam("gender") String gender, @RequestParam("page-size") String pageSize, @RequestParam("page-number") String pageNumber){
-        return ResponseEntity.ok(userService.getStaffData(name, department, primaryLanguage, specialization, gender, pageSize, pageNumber));
+        return ResponseEntity.ok(userService.findDoctor(name, department, primaryLanguage, specialization, gender, pageSize, pageNumber));
+    }
+
+    @GetMapping("/staff/id")
+    public ResponseEntity<?> getDoctorWithID(@RequestParam("id") String staffID){
+        return ResponseEntity.ok(userService.findDoctorWithID(staffID));
+    }
+
+    @PostMapping("/appointment")
+    public ResponseEntity<?> makeAppointment(@RequestBody AppointmentDTO appointmentDTO){
+        var msg = userService.makeAppointment(appointmentDTO);
+        return ResponseEntity.ok(msg);
     }
 }
